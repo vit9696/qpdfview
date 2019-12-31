5bba882dc71873fc647a1da8a5ea45115055daae
vit9696 <vit9696@users.noreply.github.com>
2020-01-01 01:32:09 +0300

    Initial version

---
+++ README.md
@@ -0,0 +1,21 @@
+qpdfview
+========
+
+Unofficial build scripts and patches for [qpdfview](https://launchpad.net/qpdfview) document viewer for macOS. Windows binaries can be found at [darealshinji/qpdfview](https://github.com/darealshinji/qpdfview).
+
+## Known issues
+
+- Copying Russian text in PDFs with CP1251-encoded data may return CP1250-interpreted data.
+  As a workaround there is an extra menu option to do this.
+- Poppler is preferred over mupdf when available.
+  As a workaround one can delete `qpdfview.app/Contents/MacOS/libqpdfview_pdf.dylib`.
+- Presentation view and fullscreen may not work as expected.
+- Open recent menu is not implemented.
+
+## Compilation requirements
+
+- Be on macOS 10.13 or newer.
+- Install [Xcode](https://developer.apple.com/xcode) and [MacPorts](https://www.macports.org).
+- Install the following ports: `bzr qt5 mupdf libspectre poppler-qt5 djvulibre imagemagick`
+- Execute `qpdfview.sh` from terminal.
+
---
+++ qpdfview.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+SELF_DIR=$(dirname "$0")
+pushd "$SELF_DIR" &>/dev/null
+SELF_DIR="$(pwd)"
+popd &>/dev/null
+
+WORK_DIR="$(pwd)"
+QPDFVIEW_DIR="${WORK_DIR}/dist"
+QPDFVIEW_APP="${QPDFVIEW_DIR}/qpdfview.app"
+QPDFVIEW_REV=2094
+QPDFVIEW_EDITION=a
+
+# Add Qt tools to PATH
+export PATH="$PATH:/opt/local/libexec/qt5/bin"
+
+check_depedencies() {
+  echo "Checking dependencies..."
+  for tool in bzr qmake make convert macdeployqt patch; do
+    if [ "$(which ${tool})" = "" ]; then
+      echo "${tool} is missing"
+      exit 1
+    fi
+  done
+}
+
+obtain_sources() {
+  echo "Obtaining sources..."
+  if [ -d "${QPDFVIEW_DIR}" ]; then
+    echo "Assuming done!"
+    return
+  fi
+
+  bzr branch lp:qpdfview dist -r "${QPDFVIEW_REV}" || exit 1
+
+  pushd "${QPDFVIEW_DIR}" &>/dev/null || exit 1
+
+  for patch in "${SELF_DIR}/patches"/*.diff; do
+    echo "Applying ${patch}"
+    patch -p0 < "${patch}" || exit 1
+  done
+
+  popd &>/dev/null || exit 1
+}
+
+compile_bundle() {
+  echo "Compiling bundle..."
+  rm -rf "${QPDFVIEW_APP}" || exit 1
+  pushd "${QPDFVIEW_DIR}" &>/dev/null || exit 1
+  qmake CONFIG+=with_fitz qpdfview.pro || exit 1
+  make -j || exit 1
+  popd &>/dev/null || exit 1
+}
+
+bundle_plugins() {
+  echo "Bundling plugins..."
+  rm -rf "${QPDFVIEW_APP}/Contents/MacOS"/*.dylib || exit 1
+  cp "${QPDFVIEW_DIR}"/*.dylib "${QPDFVIEW_APP}/Contents/MacOS" || exit 1
+}
+
+bundle_translations() {
+  echo "Bundling translation files..."
+  local tl_dir="${QPDFVIEW_APP}/Contents/Resources/data"
+  rm -rf "${tl_dir}" || exit 1
+  mkdir -p "${tl_dir}" || exit 1
+  for tl in "${QPDFVIEW_DIR}/translations"/*.ts; do
+    tl_file=$(basename "${tl/.ts/.qm}")
+    lconvert -i "${tl}" -o "${tl_dir}/${tl_file}" || exit 1
+  done
+}
+
+bundle_help() {
+  echo "Bundling help data..."
+  local help_dir="${QPDFVIEW_APP}/Contents/Resources/help"
+  rm -rf "${help_dir}" || exit 1
+  cp -r "${QPDFVIEW_DIR}/help" "${help_dir}" || exit 1
+}
+
+bundle_icon() {
+  echo "Bundling icon file..."
+  local infile="${QPDFVIEW_DIR}/icons/qpdfview.svg"
+  local outfile="${QPDFVIEW_DIR}/icons/qpdfview.icns"
+  local dest="${outfile}.iconset"
+  rm -rf "${dest}" "${outfile}" || exit 1
+  mkdir "${dest}" || exit 1
+
+  convert -background none -size '!16x16' "$infile" "$dest/icon_16x16.png" || exit 1
+  convert -background none -size '!32x32' "$infile" "$dest/icon_16x16@2x.png" || exit 1
+  cp "$dest/icon_16x16@2x.png" "$dest/icon_32x32.png" || exit 1
+  convert -background none -size '!64x64' "$infile" "$dest/icon_32x32@2x.png" || exit 1
+  convert -background none -size '!128x128' "$infile" "$dest/icon_128x128.png" || exit 1
+  convert -background none -size '!256x256' "$infile" "$dest/icon_128x128@2x.png" || exit 1
+  cp "$dest/icon_128x128@2x.png" "$dest/icon_256x256.png" || exit 1
+  convert -background none -size '!512x512' "$infile" "$dest/icon_256x256@2x.png" || exit 1
+  cp "$dest/icon_256x256@2x.png" "$dest/icon_512x512.png" || exit 1
+  convert -background none -size '!1024x1024' "$infile" "$dest/icon_512x512@2x.png" || exit 1
+
+  iconutil -c icns -o "${outfile}" "${dest}" || exit 1
+  rm -rf "$dest"
+
+  cp "${outfile}" "${QPDFVIEW_APP}/Contents/Resources/qpdfview.icns" || exit 1
+  /usr/libexec/PlistBuddy -c 'Delete CFBundleIconFile' "${QPDFVIEW_APP}/Contents/Info.plist" &>/dev/null
+  /usr/libexec/PlistBuddy -c 'Add CFBundleIconFile string qpdfview' "${QPDFVIEW_APP}/Contents/Info.plist" || exit 1
+}
+
+bundle_formats() {
+  echo "Bundling finder formats..."
+  /usr/libexec/PlistBuddy -c 'Delete CFBundleDocumentTypes' "${QPDFVIEW_APP}/Contents/Info.plist" &>/dev/null
+  /usr/libexec/PlistBuddy -c "Merge ${SELF_DIR}/formats.plist" "${QPDFVIEW_APP}/Contents/Info.plist" || exit 1
+}
+
+deploy_bundle() {
+  echo "Deploying bundle..."
+  /usr/libexec/PlistBuddy -c 'Delete CFBundleIdentifier' "${QPDFVIEW_APP}/Contents/Info.plist" &>/dev/null
+  /usr/libexec/PlistBuddy -c 'Add CFBundleIdentifier string as.vit9696.qpdfview' "${QPDFVIEW_APP}/Contents/Info.plist" || exit 1
+  macdeployqt "${QPDFVIEW_APP}" || exit 1
+}
+
+archive_bundle() {
+  echo "Archiving bundle..."
+  pushd "${QPDFVIEW_DIR}" &>/dev/null || exit 1
+
+  local version=$(cat qpdfview.pri | grep APPLICATION_VERSION | sed 's/.*= //')
+  local name="qpdfview-${version}-r${QPDFVIEW_REV}${QPDFVIEW_EDITION}.zip"
+  # codesign -s "Apple Developer" --deep qpdfview.app || exit 1
+  zip -qry ../"${name}" qpdfview.app || exit 1
+  popd &>/dev/null || exit 1
+}
+
+check_depedencies
+obtain_sources
+compile_bundle
+bundle_plugins
+bundle_translations
+bundle_help
+bundle_icon
+bundle_formats
+deploy_bundle
+archive_bundle
